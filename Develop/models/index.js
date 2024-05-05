aa26033bc1c4a6d44d644ee17d827162381bd771
Kyle <dahaydnator@gmail.com>
2024-05-05 17:11:40 -0500

    established associations in index.js for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through:{
+    model: ProductTag,
+    unique: false
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product);
 
 module.exports = {
   Product,
